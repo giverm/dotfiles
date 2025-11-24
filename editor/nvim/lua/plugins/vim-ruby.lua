0ea2107620185dba69bce19a610b986fb63d368d
Mike Giver <mikegiver@navapbc.com>
2025-11-24 10:06:22 -0500

    Update vim-ruby config to not indent when '.' is typed

--- editor/nvim/lua/plugins/vim-ruby.lua
+++ editor/nvim/lua/plugins/vim-ruby.lua
@@ -1,1 +1,12 @@
-return { "vim-ruby/vim-ruby" }
+return {
+  "vim-ruby/vim-ruby",
+  config = function()
+    -- Remove '.' from indentkeys to prevent re-indenting on dot
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "ruby",
+      callback = function()
+        vim.opt_local.indentkeys:remove(".")
+      end,
+    })
+  end,
+}
